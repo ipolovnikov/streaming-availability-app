4303f59b1c89e3c017b5207ed2d70aeb429168e0
Ivan Polovnikov <ivan@polovnikov.com>
2025-01-14 15:06:12 +0200

    feat: init

---
+++ src/app/page.tsx
@@ -0,0 +1,17 @@
+import { Metadata } from "next"
+
+export const metadata: Metadata = {
+  title: "Streaming Availability App",
+  description: "Just find your favorite shows",
+}
+
+export default async function Page() {
+  return (
+    <div className="flex min-h-screen items-center justify-center">
+      <div className="px-8">
+        <h1 className="text-5xl font-bold">{metadata.title?.toString()}</h1>
+        <p className="mt-4 text-xl font-light">{metadata.description?.toString()}</p>
+      </div>
+    </div>
+  )
+}
